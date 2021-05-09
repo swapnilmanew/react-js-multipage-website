3ed0284dcdc382f94287909f4e323fdd862bebf7
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-09 08:15:21 +0000

    asdf

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,12 @@
-import './App.css';
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Services from "./Pages/Services";
+
+import './App.css';
+
 function App() {
   return (
     <div>

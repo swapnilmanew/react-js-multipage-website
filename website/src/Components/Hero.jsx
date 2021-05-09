eb5e11439c5b226a18fea43a76e99c9ccc16d004
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-09 08:03:15 +0000

    asdf

---
+++ website/src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-10 col-md-12 m-auto">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero

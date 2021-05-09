1a011b2821ca71d9f21821f6cfe6f23ca005e10d
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-09 13:41:49 +0000

    asfd

--- website/src/Components/Cards.jsx
+++ website/src/Components/Cards.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Cards = ({ cardText, cardTitle, img }) => {
+const Cards = ({ cardText, cardTitle, cardImg }) => {
     return (
         <div className="col-12 col-sm-6 col-md-4 m-auto">
-            <div className="card border-0 shadow">
+            <div className="card border-0 shadow text-center">
+            <img src={cardImg} alt={cardImg} className="card-img-top" />
+
                 <div className="card-body">
-                    <img src="" alt="" className="card-img-top" />
                     <div className="card-title">
                         <h2 className="fw-bold text-center">{cardTitle}</h2>
                         <hr />

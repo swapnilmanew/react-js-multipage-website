b7586d0e1a2c1675755ac4dcbe5efc8b27e337af
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-09 11:04:35 +0000

    images

--- website/src/Components/Hero.jsx
+++ website/src/Components/Hero.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-const Hero = () => {
+const Hero = (props) => {
     return (
         <div class="hero-bg">
             <Navbar />
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-10 col-md-12 m-auto">
-                        <h1 className="fw-bold display-2 text-center m-auto">Hero Section</h1>
+                        <h1 className="fw-bold display-2 text-center m-auto">{props.text}</h1>
                     </div>
                 </div>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L160,288L320,224L480,64L640,288L800,192L960,288L1120,256L1280,224L1440,64L1440,320L1280,320L1120,320L960,320L800,320L640,320L480,320L320,320L160,320L0,320Z"></path></svg>
         </div>
     )
 }
--- website/src/Pages/Home.jsx
+++ website/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../Components/Hero'
 const Home = () => {
     return (
         <>
-            <Hero />
+            <Hero text="Homepage" img={} />
         </>
     )
 }

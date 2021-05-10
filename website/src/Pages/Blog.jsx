b62c7d4f3a65f6289638a5a3d43ec7ad4e5dad90
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-10 05:45:59 +0000

    sadf

--- website/src/Pages/Blog.jsx
+++ website/src/Pages/Blog.jsx
@@ -4,16 +4,23 @@ import { NavLink } from "react-router-dom";
 const Blog = ({ img, title, desc }) => {
     return (
         <>
-            <div className="col-12 col-sm-6 col-md-6 m-auto">
+            <div className="col-12 col-sm-6 col-md-6 mx-auto my-5">
                 <div className="card border-0">
-                    <div className="card-body">
-                        <h2 className="fw-bold">
-                            {title}
-                        </h2>
-                        <p className="text-muted">
-                            {desc}
-                        </p>
-                        <NavLink to="/" className="nav-link">Read More</NavLink>
+                    <div className="row">
+                        <div className="col-6 m-auto">
+                            <img src={img} alt={img} className="img-fluid" />
+                        </div>
+                        <div className="col-6 m-auto"> <div className="card-body">
+                            <h5>
+                                {title}
+                            </h5>
+                            <p className="text-muted">
+                                {desc}
+                            </p></div>
+                        </div>
+                        <div className="text-center">
+                            <NavLink to="/" className="nav-link">Read More</NavLink>
+                        </div>
 
                     </div>
                 </div>
